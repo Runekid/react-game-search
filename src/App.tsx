867b5b3cf28e8d88218fbfa307645a91f1108405
Runekid <bugdayci.onur@aol.com>
2023-04-12 16:18:00 +0200

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["Istanbul", "Brussels", "Gent", "Paris"];
-
-  const handleSelectItem = (item: string) => console.log(item);
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
