b1ec479cbd2b24a93a6ae6f545528f7de9c85ab2
Runekid <bugdayci.onur@aol.com>
2023-04-12 16:51:07 +0200

    showing an alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 
 function App() {
 
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Button color='secondary' onClick={() => console.log('clicked')}>
+      {alertVisible && <Alert onClose={() => setAlertVisible(false)}>My alert</Alert>}
+      <Button color='secondary' onClick={() => setAlertVisible(true)}>
         Primary
       </Button>
     </div>
