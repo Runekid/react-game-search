45fdf5b660678025dfa3ba131aceda9a87a0bcd1
Runekid <bugdayci.onur@aol.com>
2023-04-12 16:35:03 +0200

    building a button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+
 
 function App() {
 
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button color='secondary' onClick={() => console.log('clicked')}>
+        Primary
+      </Button>
     </div>
   );
 }
